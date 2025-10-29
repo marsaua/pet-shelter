675c3f08f96e41d6e53db3b3de8e7797d9f8a7b9
marsaua001 <marsaua001@gmail.com>
2025-10-29 16:31:40 +0200

    fix

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,7 +1,7 @@
 class UserMailer < ApplicationMailer
     default from: "notification@example.com"
 
-    def welcome(user)
+    def welcome
         @user = params[:user]
         mail to: @user.email, subject: "Welcome to Petshelter"
       end
